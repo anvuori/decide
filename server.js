82032052fa366becd2a80917851711344238d4f8
Anna Vuori <anvuori@gmail.com>
2018-04-04 16:11:29 +0300

    Adds a simple mocha test Signed-off-by: Anna Vuori <anvuori@gmail.com>

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.static(__dirname + '/public'));
 
 /* Chooses one from the options given with a post request {[name, id], [name2, id2], ...} */
 app.post('/chooseOne', function (req, res) {
-	console.log(req.body);
 	var options = req.body;
 	var rand = options[Math.floor(Math.random() * options.length)];
     res.send(JSON.stringify(rand));  
 });
 
+
+//testing !
+module.exports = app;
